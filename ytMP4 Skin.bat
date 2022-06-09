b8df5661877edd24af94e80661c13d84882fb409
Nelib <nelibcontact.pro@gmail.com>
2022-06-10 00:09:04 +0200

    Version interface

--- ytMP4 Skin.bat
+++ ytMP4 Skin.bat
@@ -1,7 +1,6 @@
 @echo off
 setlocal EnableDelayedExpansion
 :start
-del bin\url.tmp > nul: & del bin\ttl.tmp > nul:
 if not exist bin exit
 powershell -Command^ "& {Add-Type -AssemblyName Microsoft.VisualBasic; [Microsoft.VisualBasic.Interaction]::InputBox('Collez le lien de votre video ici:', 'Convertisseur YouTube vers MP4')}">bin\url.tmp & set /p url=<bin\url.tmp
 if "%url%"=="" exit
@@ -10,7 +9,7 @@ powershell -Command^ "& {Add-Type -AssemblyName System.Windows.Forms; Add-Type -
 bin\yt-dlp.exe --update
 if not exist exports mkdir exports
 bin\yt-dlp.exe --get-title %url%>bin\ttl.tmp & set /p ttl=<bin\ttl.tmp
-del bin\url.tmp > nul: & del bin\ttl.tmp > nul:
+del bin\url.tmp & del bin\ttl.tmp
 if exist "exports\%ttl%.mp4" powershell -Command^ "& {Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('Un fichier du meme nom existe deja.', 'ytMP4', 'OK', [System.Windows.Forms.MessageBoxIcon]::Warning)}" & goto start
 bin\yt-dlp.exe %url% --geo-bypass -f bestvideo+bestaudio --merge-output-format mp4 -o "exports\%ttl%.mp4"
 if exist "exports\%ttl%.mp4" powershell -Command^ "& {Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('Conversion reussie :P', 'Convertisseur YouTube vers MP4', 'OK', [System.Windows.Forms.MessageBoxIcon]::Information)}" & explorer exports
