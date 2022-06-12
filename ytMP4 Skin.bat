065302e914ca68f5f551a0ec74a617b1b477fffc
Nelib <nelibcontact.pro@gmail.com>
2022-06-12 18:50:03 +0200

    Version interface

--- ytMP4 Skin.bat
+++ ytMP4 Skin.bat
@@ -1,10 +1,10 @@
 @echo off
 setlocal EnableDelayedExpansion
-if not exist bin powershell -Command^ "& {Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('Le dossier bin est introuvable. Vous pouvez le trouver ici: https://github.com/NelibYT/ytMP4/releases', 'ytMP4 Skin', 'OK', [System.Windows.Forms.MessageBoxIcon]::Warning)}" & exit
+if not exist bin\* powershell -Command^ "& {Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('Le dossier bin est introuvable. Vous pouvez le trouver ici: https://github.com/NelibYT/ytMP4/releases', 'ytMP4 Skin', 'OK', [System.Windows.Forms.MessageBoxIcon]::Warning)}" & exit
 if exist bin\url.tmp del bin\url.tmp & if exist bin\ttl.tmp del bin\ttl.tmp
 powershell -Command^ "& {Add-Type -AssemblyName Microsoft.VisualBasic; [Microsoft.VisualBasic.Interaction]::InputBox('Collez le lien de votre video ici:', 'Convertisseur YouTube vers MP4')}">bin\url.tmp & set /p url=<bin\url.tmp
 if "%url%"=="" exit
-if not exist bin exit
+if not exist bin\* powershell -Command^ "& {Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('Le dossier bin est introuvable. Vous pouvez le trouver ici: https://github.com/NelibYT/ytMP4/releases', 'ytMP4 Skin', 'OK', [System.Windows.Forms.MessageBoxIcon]::Warning)}" & exit
 powershell -Command^ "& {Add-Type -AssemblyName System.Windows.Forms; Add-Type -AssemblyName System.Drawing; $notify = New-Object System.Windows.Forms.NotifyIcon; $notify.Icon = [System.Drawing.SystemIcons]::Information; $notify.Visible = $true; $notify.ShowBalloonTip(0, 'ytMP4 Skin', 'Conversion en cours...', [System.Windows.Forms.ToolTipIcon]::None)}"
 bin\yt-dlp.exe --update
 if not exist exports mkdir exports
