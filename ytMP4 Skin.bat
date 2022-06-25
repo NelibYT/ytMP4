a17be627041348d32b246562744817d757c86a48
Nelib <nelibcontact.pro@gmail.com>
2022-06-25 11:58:56 +0200

    Update ytMP4 Skin.bat

--- ytMP4 Skin.bat
+++ ytMP4 Skin.bat
@@ -1,4 +1,4 @@
-@echo off & title ytMP4 Skin 1.2
+title ytMP4 Skin 1.2
 ::Si les ressources sont introuvables, un message d'erreur apparaît
 if not exist "bin\yt-dlp.exe" powershell -command "& {Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('Le dossier bin est introuvable ou incomplet. Vous pouvez le trouver ici: https://github.com/NelibYT/ytMP4/releases', 'ytMP4 Skin 1.2', 'OK', [System.Windows.Forms.MessageBoxIcon]::Warning)}" & exit
 if not exist "bin\ffmpeg.exe" powershell -command "& {Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('Le dossier bin est introuvable ou incomplet. Vous pouvez le trouver ici: https://github.com/NelibYT/ytMP4/releases', 'ytMP4 Skin 1.2', 'OK', [System.Windows.Forms.MessageBoxIcon]::Warning)}" & exit
@@ -8,9 +8,9 @@ if not exist "bin\kix32.exe" powershell -command "& {Add-Type -AssemblyName Syst
 ::Si la connexion ne fonctionne pas, un message d'erreur apparaît
 ping www.youtube.com -n 1 -w 1000 >nul & if errorlevel 1 powershell -command "& {Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('ytMP4 Skin ne peut pas se connecter a internet. Verifiez votre connexion.', 'ytMP4 Skin 1.2', 'OK', [System.Windows.Forms.MessageBoxIcon]::Warning)}" & exit
 ::J'utilise curl et swissfileknife pour extraire le tag de la dernière release
-"bin\curl.exe" -k --silent "https://api.github.com/repos/NelibYT/ytMP4/releases/latest">"bin\maj.tmp" & "bin\sfk.exe" filter "bin\maj.tmp" -quiet -+tag_name -write -yes & set /p maj=<"bin\maj.tmp" & del "bin\maj.tmp"
+"bin\curl.exe" -k "https://api.github.com/repos/NelibYT/ytMP4/releases/latest">"bin\maj.tmp" & "bin\sfk.exe" filter "bin\maj.tmp" -+tag_name -write -yes & set /p maj=<"bin\maj.tmp" & del "bin\maj.tmp"
 ::Si la version actuelle n'est pas celle présente sur GitHub, on peut la télécharger
-if not "%maj:~15,-2%"=="1.3" powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('Une nouvelle version de ytMP4 Skin est disponible. Voulez-vous la telecharger?', 'ytMP4 Skin 1.2', 'YesNo', [System.Windows.Forms.MessageBoxIcon]::Information);}">"bin\dld.tmp" & set /p dld=<"bin\dld.tmp" & del "bin\dld.tmp"
+if not "%maj:~15,-2%"=="1.2" powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('Une nouvelle version de ytMP4 Skin est disponible. Voulez-vous la telecharger?', 'ytMP4 Skin 1.2', 'YesNo', [System.Windows.Forms.MessageBoxIcon]::Information);}">"bin\dld.tmp" & set /p dld=<"bin\dld.tmp" & del "bin\dld.tmp"
 if "%dld%"=="Yes" start https://github.com/NelibYT/ytMP4/releases & exit
 ::Je cache la console avec kixtart
 echo setconsole("hide")>"bin\tmp.kix" & "bin\kix32.exe" "bin\tmp.kix" & del "bin\tmp.kix"
